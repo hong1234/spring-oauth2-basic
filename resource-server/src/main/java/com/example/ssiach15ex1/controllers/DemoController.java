b8adeb08134677749b5d2af529981576c158c760
hong le <vuanhde@yahoo.de>
2025-11-19 00:14:31 +0100

    3.commit

--- resource-server/src/main/java/com/example/ssiach15ex1/controllers/DemoController.java
+++ resource-server/src/main/java/com/example/ssiach15ex1/controllers/DemoController.java
@@ -1,5 +1,6 @@
 package com.hong.resource.controllers;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class DemoController {
     @GetMapping("/demo")
     public String demo() {
         return "Demo";
+    }
+
+    @GetMapping("/teams")
+    public List<String> getTeams() {
+        return List.of("Argentina", "Australia", "Brazil", "Canada", "China PR", "Colombia", "Costa Rica", "Denmark", "England", "France", "Germany", "Italy", "Jamaica", "Japan", "Korea Republic", "Morocco", "Netherlands", "New Zealand", "Nigeria", "Norway", "Panama", "Philippines", "Portugal", "Republic of Ireland", "South Africa", "Spain", "Sweden", "Switzerland", "USA", "Vietnam", "Zambia");
     }
 }
